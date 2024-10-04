4f0902aebcc48bbbca5dab3087d5701699713845
mosesdvarghese <accydent@gmail.com>
2024-10-04 23:56:59 +0400

    Initial Commit

---
+++ Barp/BarpApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarpApp.swift
+//  Barp
+//
+//  Created by Moses Varghese on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BarpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
